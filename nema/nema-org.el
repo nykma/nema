497661a3469f80b9665a690da2d3de83387e94be
Nyk Ma <i@nyk.ma>
2018-05-16 18:19:11 +0800

    [#] org-protocol

--- nema/nema-org.el
+++ nema/nema-org.el
@@ -13,7 +13,37 @@
   :bind (("C-c a" . org-agenda)
          ("C-c c" . org-capture))
   :config
-  (require 'org-install)
+  ;; The file org-install is obsolete.
+  ;; (require 'org-install)
+
+  ;; "org-protocol:/sub-protocol:/" triggers actions associated with sub-protocol through the custom variable org-protocol-protocol-alist.
+  ;; Linux setup (Gnome)
+
+  ;; For this to work, you'll need the Gnome-Libraries to be installed.
+
+  ;; gconftool-2 -s /desktop/gnome/url-handlers/org-protocol/command '/usr/local/bin/emacsclient %s' --type String
+  ;; gconftool-2 -s /desktop/gnome/url-handlers/org-protocol/enabled --type Boolean true
+
+  ;; Linux setup (KDE)
+
+  ;; Add a file org.protocol to ~/.kde/share/kde4/services/:
+
+  ;; # -*- conf -*-
+  ;; [Protocol]
+  ;; protocol=org-protocol
+  ;; exec=/usr/bin/emacsclient '%u'
+  ;; input=none
+  ;; output=none
+  ;; helper=true
+  ;; listing=
+  ;; reading=false
+  ;; writing=false
+  ;; makedir=false
+  ;; deleting=false
+  ;; Icon=emacs
+  ;; Description=A protocol for org-mode
+  (require 'org-protocol)
+
   ;; XeLaTeX config for CJK PDF exporting
   ;; use `Source Han Serif' for main font and `Inziu Iosevka' for monospace font
   ;; SEEALSO: https://kuanyui.github.io/2014/05/10/emacs-org-mode-xelatex-output-chinese-pdf
