446877258b1cd9bedad5b5a7321a6fbde16228e0
Nyk Ma <i@nyk.ma>
2018-08-22 14:57:50 +0800

    [#] nema-php: LSP with limitations

--- nema/nema-php.el
+++ nema/nema-php.el
@@ -8,28 +8,33 @@
   :config
   (eval-after-load 'php-mode '(require 'php-ext)))
 
-(use-package php-extras
-  :defer t
-  :config
-  (eval-after-load 'php-mode '(require 'php-extras)))
+(if nema-use-lsp
+  ;; Follow https://github.com/emacs-lsp/lsp-php#php-language-server
+  ;; for instructions of installing flixfbecker/language-server.
+    (use-package lsp-php
+    :after (php-mode lsp-mode)
+    :quelpa (lsp-php :fetcher github :repo "emacs-lsp/lsp-php")
+    :hook
+    (php-mode . lsp-mode)
+    (php-mode . lsp-php-enable)
+    :config
+    (setq
+      lsp-php-language-server-command (list "php" (expand-file-name "vendor/bin/php-language-server.php" lsp-php-server-install-dir))
+     ;; lsp-php-workspace-root-detectors '(lsp-php-root-composer-json lsp-php-root-projectile lsp-php-root-vcs ".dir-locals.el" ".project" "index.php" "robots.txt")
+     lsp-php-show-file-parse-notifications t))
 
-(use-package ac-php)
-(use-package company-php
-  :hook
-  (php-mode . (lambda ()
-                (ac-php-core-eldoc-setup)
-                (make-local-variable 'company-backends)
-                (add-to-list 'company-backends 'company-ac-php-backend))))
-;; (use-package ac-php)
-;; (if (fboundp 'company-mode)
-;;     (use-package company-php
-;;       :hook (php-mode . (lambda ()
-;;                           (ac-php-core-eldoc-setup)
-;;                           (add-to-list 'company-backends 'company-ac-php-backend)))
-;;       :config
-;;       (ac-php-core-eldoc-setup)
-;;       (make-local-variable 'company-backends)
-;;       (add-to-list 'company-backends 'company-ac-php-backend)))
+  ;; else
+  (use-package php-extras
+    :defer t
+    :config
+    (eval-after-load 'php-mode '(require 'php-extras)))
+  (use-package ac-php)
+  (use-package company-php
+    :hook
+    (php-mode . (lambda ()
+                  (ac-php-core-eldoc-setup)
+                  (make-local-variable 'company-backends)
+                  (add-to-list 'company-backends 'company-ac-php-backend)))))
 
 (provide 'nema-php)
 
