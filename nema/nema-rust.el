e9535fc5b8c1b92c115a3617553cabcb90646cfc
Nyk Ma <i@nyk.ma>
2018-08-22 11:12:18 +0800

    [+] nema-rust: LSP

--- nema/nema-rust.el
+++ nema/nema-rust.el
@@ -8,20 +8,29 @@
 (use-package toml-mode
   :mode "/\\(Cargo.lock\\|\\.cargo/config\\)\\'")
 
-(use-package racer
+(if nema-use-lsp
+    ;; Install:
+    ;; rustup component add rls-preview rust-analysis rust-src
+    (use-package lsp-rust
+      :commands (lsp-rust-enable)
+      :hook (rust-mode . lsp-rust-enable))
+  ;; else
+  (use-package racer
   :defer t
   :requires (rust-mode)
   :hook ((rust-mode . racer-mode)
          (racer-mode . eldoc-mode)))
+  (use-package flycheck-rust
+    :requires (rust-mode flycheck)
+    :hook (flycheck-mode . flycheck-rust-setup)
+  ;; :config
+  ;; (with-eval-after-load 'rust-mode
+  ;;   (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+  ))
+
 
 (use-package cargo
   :requires (rust-mode))
-
-(use-package flycheck-rust
-  :requires (rust-mode flycheck)
-  :config
-  (with-eval-after-load 'rust-mode
-    (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)))
 
 (provide 'nema-rust)
 
