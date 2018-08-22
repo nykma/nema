a84f0593e17ee436e5966e30d8ed707ab4c60180
Nyk Ma <i@nyk.ma>
2018-08-22 14:50:46 +0800

    [|] nema-dired: Dired+ now managed by quelpa
    Finally I can get rid of /vendor !

--- nema/nema-dired.el
+++ nema/nema-dired.el
@@ -4,28 +4,29 @@
 ;;; Code:
 
 ;; SEEALSO: https://kuanyui.github.io/2014/06/21/dired-tutorial-and-essential-configs/
-(require 'dired)
-(require 'dired-x)
-(require 'dired+)
+(use-package dired
+  :ensure nil
+  :hook (dired-after-readin . dired-directory-sort)
+  :config
+  (require 'dired-x)
 
-(setq dired-dwim-target t)
+  (setq dired-dwim-target t
+        ;; Humanize file size
+        dired-listing-switches "-alh")
+  ;; Sort directories ahead of files
+  (defun dired-directory-sort ()
+    "Dired sort hook to list directories first."
+    (save-excursion
+      (let (buffer-read-only)
+        (forward-line 2) ;; beyond dir. header
+        (sort-regexp-fields t "^.*$" "[ ]*." (point) (point-max))))
+    (and (featurep 'xemacs)
+         (fboundp 'dired-insert-set-properties)
+         (dired-insert-set-properties (point-min) (point-max)))
+    (set-buffer-modified-p nil)))
 
-;; 目錄排在檔案之前。
-(defun dired-directory-sort ()
-  "Dired sort hook to list directories first."
-  (save-excursion
-    (let (buffer-read-only)
-      (forward-line 2) ;; beyond dir. header
-      (sort-regexp-fields t "^.*$" "[ ]*." (point) (point-max))))
-  (and (featurep 'xemacs)
-       (fboundp 'dired-insert-set-properties)
-       (dired-insert-set-properties (point-min) (point-max)))
-  (set-buffer-modified-p nil))
-(add-hook 'dired-after-readin-hook 'dired-directory-sort)
-
-;;使用 KB, MB 等方式顯示檔案大小（這個應該是 Unix 限定...Windows 我不
-;;知該怎麼辦）。
-(setq dired-listing-switches "-alh")
+(use-package dired+
+  :quelpa (dired+ :fetcher url :url "https://www.emacswiki.org/emacs/download/dired+.el"))
 
 (provide 'nema-dired)
 
