6be693df74210972a975cf0042def002004b7f6b
Nyk Ma <i@nyk.ma>
2018-12-11 11:38:55 +0800

    [+] nema-dart: LSP

--- nema/nema-dart.el
+++ nema/nema-dart.el
@@ -7,6 +7,13 @@
   :config
   (setq dart-enable-analysis-server t))
 
+(if nema-use-lsp
+    ;; Install dart_language_server :
+    ;; $ pub global activate dart_language_server
+    (use-package lsp-dart
+      :commands (lsp-dart-enable)
+      :hook ((dart-mode . lsp-dart-enable))))
+
 (provide 'nema-dart)
 
 ;;; nema-dart.el ends here
