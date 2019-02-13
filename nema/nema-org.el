17e38e631534d783aface389e8748bb6047a516e
Nyk Ma <i@nyk.ma>
2019-02-13 16:21:29 +0800

    [#] nema-org: configs of pdf-tools

--- nema/nema-org.el
+++ nema/nema-org.el
@@ -56,7 +56,27 @@
   (setq org-log-done 'clock))
 
 ;; PDF viewer
-(use-package pdf-tools)
+(use-package pdf-tools
+  :magic ("%PDF" . pdf-view-mode)
+  :config
+  ;; Copied from http://pragmaticemacs.com/emacs/more-pdf-tools-tweaks/
+  ;; initialise
+  (pdf-tools-install)
+  ;; open pdfs scaled to fit page
+  (setq-default pdf-view-display-size 'fit-page)
+  ;; automatically annotate highlights
+  ;; (setq pdf-annot-activate-created-annotations t)
+  ;; use normal isearch
+  ;; (define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward)
+  ;; turn off cua so copy works
+  (add-hook 'pdf-view-mode-hook (lambda () (cua-mode 0)))
+  ;; more fine-grained zooming
+  (setq pdf-view-resize-factor 1.1)
+  ;; keyboard shortcuts
+  (define-key pdf-view-mode-map (kbd "h") 'pdf-annot-add-highlight-markup-annotation)
+  (define-key pdf-view-mode-map (kbd "t") 'pdf-annot-add-text-annotation)
+  (define-key pdf-view-mode-map (kbd "D") 'pdf-annot-delete))
+
 
 (use-package org-plus-contrib
   :pin org
