3e9e1748e6935f5489eee791b33d77f31e89d6e2
Nyk Ma <i@nyk.ma>
2019-03-06 18:21:41 +0800

    [+] my-sample/symbol-overlay

---
+++ my-sample/symbol-overlay.el
@@ -0,0 +1,10 @@
+;;; symbol-overlay --- Fast highlight keywords
+;;; Commentary:
+"Fast highlight keywords"
+;;; Code:
+
+(use-package symbol-overlay
+  :bind-keymap
+  ("C-$" . symbol-overlay-map))
+
+;;; symbol-overlay.el ends here
