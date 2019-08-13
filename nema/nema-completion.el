5f25dfd13b5dfbbfc013c407eb90128017a67ad0
Nyk Ma <i@nyk.ma>
2019-08-13 11:23:25 +0800

    [#] nema-completion: reduce delay of quickhelp

--- nema/nema-completion.el
+++ nema/nema-completion.el
@@ -6,20 +6,23 @@
   :hook (after-init . global-company-mode)
   :config
   (setq company-tooltip-align-annotations t
-        company-tooltip-limit 12
+        company-tooltip-limit 20
+        company-show-numbers t
         company-idle-delay .2               ; decrease delay before autocompletion popup shows
         company-echo-delay 0                ; remove annoying blinking
         company-minimum-prefix-length 2
         company-require-match nil)
+
   (defun nema--company-backend-with-yas (backend)
     (append (if (consp backend) backend (list backend))
             '(:with company-yasnippet)))
+
   ;; Popup documentation for completion candidates
   (use-package company-quickhelp
     ;; :bind (:map company-active-map
     ;; ("M-h" . company-quickhelp-manual-begin))
     :hook (global-company-mode . company-quickhelp-mode)
-    :config (setq company-quickhelp-delay 0.5)))
+    :config (setq company-quickhelp-delay 0.2))
 
 (provide 'nema-completion)
 
