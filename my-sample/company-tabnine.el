482181d492b1f27b4c54dd01b0fad65373929f9b
Nyk Ma <i@nyk.ma>
2019-08-29 12:30:43 +0800

    [+] my-sample/company-tabnine

---
+++ my-sample/company-tabnine.el
@@ -0,0 +1,12 @@
+;;; company-tabnine --- Tabnine backend for company
+;;; Commentary:
+"Tabnine backend for company"
+;;; Code:
+
+(use-package company-tabnine
+  :config
+  (push 'company-tabnine company-backends))
+
+(provide 'company-tabnine)
+
+;;; company-tabnine.el ends here
