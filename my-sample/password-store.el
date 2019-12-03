9eb58352013c7587c91d17d34183c48fa6bd56c3
Nyk Ma <i@nyk.ma>
2019-12-03 17:02:40 +0800

    [+] my-sample/password-store.el

---
+++ my-sample/password-store.el
@@ -0,0 +1,9 @@
+;;; password-store --- pass for Emacs
+;;; Commentary:
+"pass for Emacs"
+;;; Code:
+
+(use-package password-store
+  :quelpa (password-store :fetcher url :url "https://git.zx2c4.com/password-store/plain/contrib/emacs/password-store.el"))
+
+;;; password-store.el ends here
