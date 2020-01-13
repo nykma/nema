104bc4b22e50effc32fb9378281539ebed2f1423
Nyk Ma <i@nyk.ma>
2020-01-13 15:16:34 +0800

    [#] my-sample/telega

--- my-sample/telega.el
+++ my-sample/telega.el
@@ -1,3 +1,8 @@
 (use-package telega
+  :hook ((telega-chat-mode . telega-url-shorten-mode))
   :config
-  (setq telega-cache-dir (expand-file-name ".cache/telega" user-emacs-directory)))
+  (setq
+   telega-directory (expand-file-name ".cache/telega" user-emacs-directory)
+   telega-video-play-inline t)
+  (telega-mode-line-mode 1)
+  (telega-notifications-mode 1))
