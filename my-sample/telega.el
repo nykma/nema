bb19f26bb38d2fb32094d72e8c61299770a118eb
Nyk Ma <i@nyk.ma>
2020-01-14 02:13:23 +0800

    [#] my-sample/telega: set variables before (require)

--- my-sample/telega.el
+++ my-sample/telega.el
@@ -1,8 +1,10 @@
 (use-package telega
   :hook ((telega-chat-mode . telega-url-shorten-mode))
-  :config
+  :pin melpa-stable
+  :init
   (setq
    telega-directory (expand-file-name ".cache/telega" user-emacs-directory)
    telega-video-play-inline t)
+  :config
   (telega-mode-line-mode 1)
   (telega-notifications-mode 1))
