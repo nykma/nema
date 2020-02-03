a2d50f56d2eb097f7eca69cebfd888a8f82b6637
Nyk Ma <i@nyk.ma>
2020-02-03 17:10:57 +0800

    [!] nema-core: omit marmalade repo

--- nema/nema-core.el
+++ nema/nema-core.el
@@ -17,7 +17,7 @@
   (unless (eq old-package-archives package-archives)
     (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
     (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
-    (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+    ;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
     (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)))
 
 ;; use-package
