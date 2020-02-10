c81180a1c621131953bbcf8db88c0aa61edaeea1
Nyk Ma <i@nyk.ma>
2020-02-11 00:50:52 +0800

    [+] my-sample/ox-hugo

---
+++ my-sample/ox-hugo.el
@@ -0,0 +1,13 @@
+;;; ox-hugo --- Org exports to Hugo-compatible markdowns
+;;; Commentary:
+"Org exports to Hugo-compatible markdowns"
+;;; Code:
+
+
+(use-package ox-hugo
+  :after ox
+  :config
+  (setq org-hugo-auto-set-lastmod t
+        org-hugo-use-code-for-kbd t))
+
+;;; ox-hugo.el ends here
