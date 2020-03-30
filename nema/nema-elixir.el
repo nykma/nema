a23981eec8fe886eb84fb38cf7ffd53584093b4d
Nyk Ma <i@nyk.ma>
2020-03-30 15:51:41 +0800

    [!] nema-elixir: change alchemist base repo

--- nema/nema-elixir.el
+++ nema/nema-elixir.el
@@ -5,7 +5,12 @@
 
 (use-package elixir-mode
   :quelpa (elixir-mode :fetcher github :repo "elixir-editors/emacs-elixir"))
-(use-package alchemist)
+
+(use-package alchemist
+  ;; Since original repo is kinda deprecated
+  :quelpa (alchemist :fetcher github :repo "dfischer/alchemist.el")
+  :bind (:map alchemist-mode-map
+              ([remap alchemist-goto-definition-at-point] . lsp-ui-peek-find-definitions)))
 
 (provide 'nema-elixir)
 
