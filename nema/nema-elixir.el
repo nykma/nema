6a238034a80ec25676a98ebf200e5e8dd24d7966
Nyk Ma <i@nyk.ma>
2020-04-15 14:12:31 +0800

    [!] nema-elixir: alchemist is dead, long live LSP!

--- nema/nema-elixir.el
+++ nema/nema-elixir.el
@@ -6,11 +6,16 @@
 (use-package elixir-mode
   :quelpa (elixir-mode :fetcher github :repo "elixir-editors/emacs-elixir"))
 
+;; alchemist is dead, long live LSP!
+(use-package mix
+  :if nema-lsp
+  :quelpa (mix.el :fetcher github :repo "ayrat555/mix.el" :files ("mix.el" "LICENSE"))
+  :hook ((elixir-mode . mix-minor-mode)))
+
 (use-package alchemist
+  :if (not nema-lsp)
   ;; Since original repo is kinda deprecated
-  :quelpa (alchemist :fetcher github :repo "dfischer/alchemist.el")
-  :bind (:map alchemist-mode-map
-              ([remap alchemist-goto-definition-at-point] . lsp-ui-peek-find-definitions)))
+  :quelpa (alchemist :fetcher github :repo "dfischer/alchemist.el"))
 
 (provide 'nema-elixir)
 
