f1b4fa7a56115090d185c1925e0fa94523acc093
Nyk Ma <i@nyk.ma>
2020-05-06 12:57:56 +0800

    [+] my/cjk-font: Example for setting Emacs GUI CJK font

---
+++ my-sample/cjk-font.el
@@ -0,0 +1,16 @@
+;;; cjk-font --- Sample CJK font setting
+;;; Commentary:
+"Sample CJK font setting"
+;;; Code:
+
+(defun nema/font-init ()
+  "FONT settings of gnu-Unifont."
+  (interactive)
+  (let ((nema/cjk-font "-????-Sarasa Mono SC-normal-normal-normal-*-*-*-*-*-*-0-iso10646-1"))
+    (dolist (charset '(kana han cjk-misc bopomofo cyrillic)) ;; add `latin' if you want to change normal letter too
+      (set-fontset-font (frame-parameter nil 'font) charset
+                        (font-spec :name nema/cjk-font)))))
+
+;; (add-hook 'after-init-hook 'nema/font-init)
+
+;;; cjk-font.el ends here
