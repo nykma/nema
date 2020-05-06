0cb551a972c1a28233ae64e0b3bc60b2b99c8a21
Nyk Ma <i@nyk.ma>
2020-05-06 12:57:06 +0800

    [#] my/tree-sitter: add global-tree-sitter-mode

--- my-sample/tree-sitter.el
+++ my-sample/tree-sitter.el
@@ -9,16 +9,20 @@
 
 (use-package tree-sitter-langs
   :config
+  (global-tree-sitter-mode)
+  (require 'tree-sitter-hl)
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
 
   (add-to-list 'tree-sitter-major-mode-language-alist '(tuareg-mode . ocaml))
   (add-to-list 'tree-sitter-major-mode-language-alist '(ng2-ts-mode . typescript))
 
-  (dolist (lang tree-sitter-major-mode-language-alist) ;; (agda-mode . agda)
-    (let* ((lang-mode (car lang))
-           (lang-hook-string (format "%s-hook" lang-mode))
-           (lang-hook (intern-soft lang-hook-string)))
-      (if lang-hook
-          (add-hook lang-hook #'tree-sitter-mode)
-        (message "tree-sitter: %s not found in current emacs environment. Skipped" lang-hook-string)))))
+  ;; (dolist (lang tree-sitter-major-mode-language-alist) ;; (agda-mode . agda)
+  ;;   (let* ((lang-mode (car lang))
+  ;;          (lang-hook-string (format "%s-hook" lang-mode))
+  ;;          (lang-hook (intern-soft lang-hook-string)))
+  ;;     (if lang-hook
+  ;;         (add-hook lang-hook #'tree-sitter-mode)
+  ;;       (message "tree-sitter: %s not found in current emacs environment. Skipped" lang-hook-string))))
+  )
 
 ;;; tree-sitter.el ends here
