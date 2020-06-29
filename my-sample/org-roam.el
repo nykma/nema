8253ed82529ba25e1fbee6f910f408174096f83f
Nyk Ma <i@nyk.ma>
2020-06-29 15:46:45 +0800

    [#] my/org-roam: fix deprecated function call

--- my-sample/org-roam.el
+++ my-sample/org-roam.el
@@ -3,15 +3,17 @@
 "Rudimentary Roam replica with Org-mode"
 ;;; Code:
 
+(require 'nema-customize-group)
+
 (use-package org-roam
-  :delight ""
+  :delight org-roam-mode
   :hook (after-init . org-roam-mode)
   ;; :custom
   ;; (org-roam-directory "/path/to/org-files/")
   :bind (:map org-roam-mode-map
               (("C-c n l" . org-roam)
                ("C-c n f" . org-roam-find-file)
-               ("C-c n g" . org-roam-show-graph))
+               ("C-c n g" . org-roam-graph))
               :map org-mode-map
               (("C-c n i" . org-roam-insert)))
   :config
