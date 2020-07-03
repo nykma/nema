0feb7ce6e29401766d033591551c11c1cd0c3a26
Nyk Ma <i@nyk.ma>
2020-07-03 14:59:43 +0800

    [+] nema-dart: hover

--- nema/nema-dart.el
+++ nema/nema-dart.el
@@ -3,6 +3,8 @@
 "Dart tools"
 ;;; Code:
 
+(require 'nema-customize-group)
+
 (use-package dart-mode)
 
 (use-package flutter
@@ -10,11 +12,23 @@
   :bind (:map dart-mode-map
               ("C-M-x" . #'flutter-run-or-hot-reload))
   :config
-  (setq flutter-sdk-path "/opt/flutter/"))
+  ;;(setq flutter-sdk-path "/opt/flutter/")
+  )
 
 (use-package lsp-dart
   :if (eq nema-lsp 'lsp-mode)
   :hook (dart-mode . lsp))
+
+(use-package hover
+  :if (executable-find "hover")
+  :after dart-mode
+  :bind (:map dart-mode-map
+              ("C-M-z" . #'hover-run-or-hot-reload)
+              ("C-M-x" . #'hover-run-or-hot-restart)
+              ("C-M-p" . #'hover-take-screenshot))
+  :init
+  (setq hover-command-path (executable-find "hover")
+        hover-hot-reload-on-save t))
 
 (provide 'nema-dart)
 
