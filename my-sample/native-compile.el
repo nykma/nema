984a16a32faf206753c10784a6d5cf5004c20c0c
Nyk Ma <i@nyk.ma>
2020-07-11 20:24:02 +0800

    [#] nema/native-compile: add ignore keyword list

--- my-sample/native-compile.el
+++ my-sample/native-compile.el
@@ -3,13 +3,38 @@
 "Native compile helper function"
 ;;; Code:
 
+(require 'nema-customize-group)
+(require 's) ;; s-contains?/2
+
+(defcustom nema-native-compile-ignore-names '()
+  "Packages' name which should be ignored when native-compiling
+Default is empty list"
+  :tag "Native compile ignore list"
+  :group 'nema
+  :type '(repeat string))
+
+(defun nema//string-contains-keyword (haystack needle-list)
+  "Detect each string in NEEDLE-LIST is found in HAYSTACK string.
+
+Return t if any one in NEEDLE-LIST matches."
+  (catch 'found
+    (dolist (needle needle-list)
+      (when (s-contains? needle haystack)
+        (throw 'found t)))))
+
+(defun nema//native-compile-target-p (path)
+  "Judge whether PATH is native compilling target."
+  (and (not (nema//string-contains-keyword path nema-native-compile-ignore-names))
+       (file-directory-p path)))
+
 (defun nema/native-compile ()
   "Recursively compile elpa folder using `native-compile-async'."
   (interactive)
   (unless (fboundp 'native-compile-async)
     (error "This Emacs is not compatible with native compiling.  Should build a feature/native-comp version"))
-  (let* ((target (expand-file-name "elpa" user-emacs-directory)))
-    (native-compile-async target t)
-    (switch-to-buffer-other-window comp-async-buffer-name)))
+  (switch-to-buffer-other-window "*Async-native-compile-log*")
+  (let* ((dirs (directory-files (expand-file-name "elpa" user-emacs-directory) t "^[^\\.]"))
+         (target (seq-filter #'nema//native-compile-target-p dirs)))
+    (native-compile-async target t)))
 
 ;;; native-compile.el ends here
