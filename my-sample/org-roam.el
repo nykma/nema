2cc3e7abd555b9bc945a8ce3494dad40f5af218d
Nyk Ma <i@nyk.ma>
2020-09-06 17:35:36 +0800

    [#] org-roam: server, protocol

--- my-sample/org-roam.el
+++ my-sample/org-roam.el
@@ -21,7 +21,24 @@
   :init
   (setq org-roam-db-location (expand-file-name ".cache/roam.db" user-emacs-directory))
   :config
+  ;; https://www.orgroam.com/manual/Installation-_00281_0029.html#Installation-_00281_0029
+  (require 'org-roam-protocol)
   (setq org-roam-completion-system nema-emacs-completion-engine))
+
+(use-package org-roam-server
+  :commands (org-roam-server-mode)
+  :config
+  (setq org-roam-server-host "127.0.0.1"
+        org-roam-server-port 58080
+        org-roam-server-authenticate nil
+        org-roam-server-export-inline-images t
+        org-roam-server-serve-files nil
+        org-roam-server-served-file-extensions '("pdf" "mp4" "ogv")
+        org-roam-server-network-poll t
+        org-roam-server-network-arrows nil
+        org-roam-server-network-label-truncate t
+        org-roam-server-network-label-truncate-length 60
+        org-roam-server-network-label-wrap-length 20))
 
 (use-package org-journal
   :after org-roam
