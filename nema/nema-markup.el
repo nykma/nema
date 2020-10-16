612e9bc7ef4a84f369e1fc50263fc61d2d5cc242
Nyk Ma <i@nyk.ma>
2020-10-16 15:57:32 +0800

    [#] nema-markup: yaml-mode base64 function helper

--- nema/nema-markup.el
+++ nema/nema-markup.el
@@ -4,7 +4,16 @@
 ;;; Code:
 
 (use-package yaml-mode
-  :bind (("\C-m" . newline-and-indent)))
+  :bind ((:map yaml-mode-map
+               ("\C-m" . newline-and-indent)
+               ("C-c C-v" . nema/base64-encode-region-nowrap)
+               ("C-c C-c" . base64-decode-region)))
+  :init
+  (defun nema/base64-encode-region-nowrap ()
+    "Encode region into base64 with no linewrap.
+Useful when editing Kubernetes Secrets."
+    (interactive)
+    (base64-encode-region (point) (mark) t)))
 
 (use-package csv-mode)
 
