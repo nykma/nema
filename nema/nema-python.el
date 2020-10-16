ccd503ffe60d1b38c259323cdc94c4876102345d
Nyk Ma <i@nyk.ma>
2020-10-16 13:53:32 +0800

    [#] nema-python: pyright install command

--- nema/nema-python.el
+++ nema/nema-python.el
@@ -4,6 +4,7 @@
 (unless nema-lsp
   (use-package elpy))
 
+;; npm i -g pyright
 (use-package lsp-pyright
   :if (eq nema-lsp 'lsp-mode)
   :hook (python-mode . (lambda () (require 'lsp-pyright) (lsp))))
