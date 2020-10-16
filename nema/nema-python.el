a55bc6a2cb27218331281b5ae111acc9bf33b45d
Nyk Ma <i@nyk.ma>
2020-10-16 13:48:46 +0800

    [#] nema-python: use lsp-pyright
    Next-gen Python static checker

--- nema/nema-python.el
+++ nema/nema-python.el
@@ -4,12 +4,9 @@
 (unless nema-lsp
   (use-package elpy))
 
-(use-package lsp-python-ms
+(use-package lsp-pyright
   :if (eq nema-lsp 'lsp-mode)
-  :hook (python-mode . (lambda () (require 'lsp-python-ms) (lsp)))
-  :config
-  (setq lsp-python-ms-dir (expand-file-name ".cache/mspyls" user-emacs-directory)
-        lsp-python-ms-executable (expand-file-name ".cache/mspyls/Microsoft.Python.LanguageServer" user-emacs-directory)))
+  :hook (python-mode . (lambda () (require 'lsp-pyright) (lsp))))
 
 (provide 'nema-python)
 
