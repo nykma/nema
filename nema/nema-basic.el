b0c9c1fbdb6eb59791d3570ff604a0fcd19f75a7
Nyk Ma <i@nyk.ma>
2021-01-24 03:34:15 +0800

    [#] Smartparens instead of paredit

--- nema/nema-basic.el
+++ nema/nema-basic.el
@@ -36,7 +36,7 @@
   ;; Helm - Completion
   ('helm (require 'nema-helm))
   ('selectrum (require 'nema-selectrum))
-  (t (ido-mode 1)))
+  (_ (ido-mode 1)))
 
 ;; Interactive regex replacer
 (use-package anzu
@@ -131,10 +131,87 @@
 ;;   (global-origami-mode))
 
 ;; Minor mode for Emacs that deals with parens pairs and tries to be smart about it.
-;; (use-package smartparens
-;;   :delight
-;;   :config
-;;   (smartparens-global-mode t))
+(use-package smartparens-config
+  :ensure smartparens
+  ;; https://ebzzry.io/en/emacs-pairs/#keys
+  :hook ((prog-mode markdown-mode org-mode) . turn-on-smartparens-strict-mode)
+  :bind (:map smartparens-mode-map
+              ("C-M-a" . sp-beginning-of-sexp)
+              ("C-M-e" . sp-end-of-sexp)
+
+              ("C-<down>" . sp-down-sexp)
+              ("C-<up>"   . sp-up-sexp)
+              ("M-<down>" . sp-backward-down-sexp)
+              ("M-<up>"   . sp-backward-up-sexp)
+
+              ("C-M-f" . sp-forward-sexp)
+              ("C-M-b" . sp-backward-sexp)
+
+              ("C-M-n" . sp-next-sexp)
+              ("C-M-p" . sp-previous-sexp)
+
+              ("C-S-f" . sp-forward-symbol)
+              ("C-S-b" . sp-backward-symbol)
+
+              ("C-<right>" . sp-forward-slurp-sexp)
+              ("M-<right>" . sp-forward-barf-sexp)
+              ("C-<left>"  . sp-backward-slurp-sexp)
+              ("M-<left>"  . sp-backward-barf-sexp)
+
+              ("C-M-t" . sp-transpose-sexp)
+              ("C-M-k" . sp-kill-sexp)
+              ("C-k"   . sp-kill-hybrid-sexp)
+              ("M-k"   . sp-backward-kill-sexp)
+              ("C-M-w" . sp-copy-sexp)
+              ("C-M-d" . delete-sexp)
+
+              ("M-<backspace>" . backward-kill-word)
+              ("C-<backspace>" . sp-backward-kill-word)
+              ([remap sp-backward-kill-word] . backward-kill-word)
+
+              ("M-[" . sp-backward-unwrap-sexp)
+              ("M-]" . sp-unwrap-sexp)
+
+              ("C-x C-t" . sp-transpose-hybrid-sexp)
+
+              ("C-c ("  . nema//wrap-with-parens)
+              ("C-c ["  . nema//wrap-with-brackets)
+              ("C-c {"  . nema//wrap-with-braces)
+              ("C-c '"  . nema//wrap-with-single-quotes)
+              ("C-c \"" . nema//wrap-with-double-quotes)
+              ("C-c _"  . nema//wrap-with-underscores)
+              ("C-c `"  . nema//wrap-with-back-quotes))
+  :config
+  (show-smartparens-global-mode t)
+  (smartparens-strict-mode t)
+
+  (defmacro nema//def-pairs (pairs)
+    "Define functions for pairing. PAIRS is an alist of (NAME . STRING)
+conses, where NAME is the function name that will be created and
+STRING is a single-character string that marks the opening character.
+
+  (def-pairs ((paren . \"(\")
+              (bracket . \"[\"))
+
+defines the functions WRAP-WITH-PAREN and WRAP-WITH-BRACKET,
+respectively."
+    `(progn
+       ,@(loop for (key . val) in pairs
+               collect
+               `(defun ,(read (concat
+                               "nema//wrap-with-"
+                               (prin1-to-string key)
+                               "s"))
+                    (&optional arg)
+                  (interactive "p")
+                  (sp-wrap-with-pair ,val)))))
+
+  (nema//def-pairs ((paren . "(")
+                    (bracket . "[")
+                    (brace . "{")
+                    (single-quote . "'")
+                    (double-quote . "\"")
+                    (back-quote . "`"))))
 
 ;; Popup window manager
 ;; Seldom use it. Disabled.
--- nema/nema-elisp.el
+++ nema/nema-elisp.el
@@ -3,17 +3,18 @@
 "LISP enhance tools."
 ;;; Code:
 
-(use-package paredit
-  :delight
-  :config
-  ;; SEEALSO: https://www.emacswiki.org/emacs/ParEdit
-  (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
-  (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
-  (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-  (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
-  (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
-  (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
-  (add-hook 'scheme-mode-hook           #'enable-paredit-mode))
+;; Deprecated. Use smartparens instead.
+;; (use-package paredit
+;;   :delight
+;;   :config
+;;   ;; SEEALSO: https://www.emacswiki.org/emacs/ParEdit
+;;   (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+;;   (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+;;   (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+;;   (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+;;   (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+;;   (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+;;   (add-hook 'scheme-mode-hook           #'enable-paredit-mode))
 
 (provide 'nema-elisp)
 
