123daf32dbf5e76ee99b659eef6a343992da557c
Nyk Ma <i@nyk.ma>
2021-03-03 13:58:15 +0800

    [#] my/native-compile: match current upstream condition

--- my-sample/native-compile.el
+++ my-sample/native-compile.el
@@ -1,42 +1,11 @@
-;;; native-compile --- Native compile support and helper function
+;;; native-compile --- native-comp support and helper
 ;;; Commentary:
-"Native compile helper function"
+"Configs for native-comp branch"
 ;;; Code:
 
-(require 'nema-customize-group)
-(require 's) ;; s-contains?/2
-
-(defcustom nema-native-compile-ignore-names '("telega")
-  "Packages' name which should be ignored when native-compiling.
-
-Default is '(\"telega\")"
-  :tag "Native compile ignore list"
-  :group 'nema
-  :type '(repeat string))
-
-(defun nema//string-contains-keyword (haystack needle-list)
-  "Detect each string in NEEDLE-LIST is found in HAYSTACK string.
-
-Return t if any one in NEEDLE-LIST matches."
-  (catch 'found
-    (dolist (needle needle-list)
-      (when (s-contains? needle haystack)
-        (throw 'found t)))))
-
-(defun nema//native-compile-target-p (path)
-  "Judge whether PATH is native compilling target."
-  (and (not (nema//string-contains-keyword path nema-native-compile-ignore-names))
-       (file-directory-p path)))
-
-(defun nema/native-compile ()
-  "Recursively compile elpa folder using `native-compile-async'."
-  (interactive)
-  (unless (fboundp 'native-compile-async)
-    (error "This Emacs is not compatible with native compiling.  Should build a feature/native-comp version"))
-  (switch-to-buffer-other-window "*Async-native-compile-log*")
-  (let* ((dirs (directory-files (expand-file-name "elpa" user-emacs-directory) t "^[^\\.]"))
-         (target (seq-filter #'nema//native-compile-target-p dirs)))
-    (native-compile-async target t)))
+;; 20210303: native-comp generates too much warnings. Disable it.
+(if (fboundp 'native-compile-async)
+    (setq warning-minimum-level :emergency))
 
 ;; Maybe a workaround of
 ;; Error: Symbol’s function definition is void: cc-bytecomp-is-compiling
