e3ddeccadf66e2abd3be6c409a57463d57b04ada
Nyk Ma <i@nyk.ma>
2021-05-26 17:30:39 +0800

    [+] my/matrix

---
+++ my-sample/matrix.el
@@ -0,0 +1,3 @@
+(use-package matrix-client
+  :quelpa (matrix-client :fetcher "github" :repo "alphapapa/matrix-client.el"
+                         :files (:defaults "logo.png" "matrix-client-standalone.el.sh")))
