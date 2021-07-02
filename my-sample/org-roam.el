60e6076d59a06d208fb4070564385d49748109c4
Nyk Ma <i@nyk.ma>
2021-07-02 15:56:13 +0800

    [#] my/org-roam: add roam_key in capture template

--- my-sample/org-roam.el
+++ my-sample/org-roam.el
@@ -28,7 +28,7 @@
         '(("d" "default" plain (function org-roam-capture--get-point)
            "%?"
            :file-name "%<%Y%m%d%H%M%S>-${slug}"
-           :head "#+title: ${title}\n#+roam_alias:\n#+roam_tags:\n\n* ${title}\n"))
+           :head "#+title: ${title}\n#+roam_alias:\n#+roam_key:\n#+roam_tags:\n\n* ${title}\n"))
         org-roam-capture-ref-templates
         ;; webclip JS bookmark:
         ;; javascript:location.href = 'org-protocol://roam-ref?template=c&ref=' + encodeURIComponent(location.href) + '&title='+encodeURIComponent(document.title) + '&body='+encodeURIComponent(function(){var html = "";var sel = window.getSelection();if (sel.rangeCount) {var container = document.createElement("div");for (var i = 0, len = sel.rangeCount; i < len; ++i) {container.appendChild(sel.getRangeAt(i).cloneContents());}html = container.innerHTML;}var dataDom = document.createElement('div');dataDom.innerHTML = html;['p', 'h1', 'h2', 'h3', 'h4'].forEach(function(tag, idx){dataDom.querySelectorAll(tag).forEach(function(item, index) {var content = item.innerHTML.trim();if (content.length > 0) {item.innerHTML = content + '&#13;&#10;';}});});return dataDom.innerText.trim();}())
