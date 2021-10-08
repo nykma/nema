fae79de6fd01a70bb5bad0e62ae61a95f8ca4db3
Nyk Ma <i@nyk.ma>
2021-10-08 13:41:45 +0800

    [+] nema-ivy: ivy-posframe

--- nema/nema-ivy.el
+++ nema/nema-ivy.el
@@ -35,7 +35,15 @@
     (prescient-persist-mode t))
   (use-package ivy-prescient
     :config
-    (ivy-prescient-mode t)))
+    (ivy-prescient-mode t))
+  (use-package ivy-posframe
+    :config
+    (ivy-posframe-mode 1)
+    (setq ivy-posframe-display-functions-alist
+          '((swiper          . ivy-posframe-display-at-point)
+            (complete-symbol . ivy-posframe-display-at-point)
+            (counsel-M-x     . ivy-posframe-display-at-window-bottom-left)
+            (t               . ivy-posframe-display)))))
 
 (provide 'nema-ivy)
 ;;; nema-ivy.el ends here
