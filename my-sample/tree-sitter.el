634b1b5083f8055d1f6b4c76efe6e651d1c1e7b1
Nyk Ma <i@nyk.ma>
2021-11-26 14:42:14 +0800

    [#] my/tree-sitter: the real structure editing plugin

--- my-sample/tree-sitter.el
+++ my-sample/tree-sitter.el
@@ -3,14 +3,49 @@
 "Incremental parsing"
 ;;; Code:
 
-;; tree-sitter-core
+(defvar nema--tree-sitter-grammatical-edit-lang-hooks
+  '(
+    c-mode-common-hook
+    c-mode-hook
+    c++-mode-hook
+    java-mode-hook
+    haskell-mode-hook
+    tuareg-mode-hook
+    ;; maxima-mode-hook
+    ielm-mode-hook
+    sh-mode-hook
+    ;; makefile-gmake-mode-hook
+    php-mode-hook
+    python-mode-hook
+    js-mode-hook
+    go-mode-hook
+    qml-mode-hook
+    jade-mode-hook
+    css-mode-hook
+    ruby-mode-hook
+    coffee-mode-hook
+    rust-mode-hook
+    ;; qmake-mode-hook
+    lua-mode-hook
+    swift-mode-hook
+    elixir-mode-hook
+    ;; TODO: make elisp tree-sitter plugin auto-compiled
+    ;; SEE ALSO: https://emacs-china.org/t/tree-sitter/19014
+    ;; emacs-lisp-mode-hook
+    ;; lisp-interaction-mode-hook
+    ;; lisp-mode-hook
+    ;; minibuffer-inactive-mode-hook
+    )
+  "All language mode hooks to use tree-sitter grammatical-edit (instead of puni-mode).")
 
+;; tree-sitter-core
 (use-package tree-sitter)
 
 (use-package tree-sitter-langs
   :after tree-sitter
   :config
   (require 'tree-sitter-hl)
+  (require 'tree-sitter-query)
   (global-tree-sitter-mode)
   (delight 'tree-sitter-mode nil 'tree-sitter)
   (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
@@ -25,6 +60,43 @@
   ;;         (add-hook lang-hook #'tree-sitter-mode)
   ;;       (message "tree-sitter: %s not found in current emacs environment. Skipped" lang-hook-string))))
   )
+
+;; Structure editing based on tree-sitter
+(use-package grammatical-edit
+  :quelpa (grammatical-edit :fetcher github :repo "manateelazycat/grammatical-edit")
+  :bind ((:map grammatical-edit-mode-map
+               ("(" . grammatical-edit-open-round)
+               ("[" . grammatical-edit-open-bracket)
+               ("{" . grammatical-edit-open-curly)
+               (")" . grammatical-edit-close-round)
+               ("]" . grammatical-edit-close-bracket)
+               ("}" . grammatical-edit-close-curly)
+               ("=" . grammatical-edit-equal)
+
+               ("%" . grammatical-edit-match-paren)
+               ("\"" . grammatical-edit-double-quote)
+
+               ("SPC" . grammatical-edit-space)
+               ("RET" . grammatical-edit-newline)
+
+               ("M-o" . grammatical-edit-backward-delete)
+               ("C-d" . grammatical-edit-forward-delete)
+               ("C-k" . grammatical-edit-kill)
+
+               ("M-\"" . grammatical-edit-wrap-double-quote)
+               ("M-[" . grammatical-edit-wrap-bracket)
+               ("M-{" . grammatical-edit-wrap-curly)
+               ("M-(" . grammatical-edit-wrap-round)
+               ("M-)" . grammatical-edit-unwrap)
+
+               ("M-p" . grammatical-edit-jump-right)
+               ("M-n" . grammatical-edit-jump-left)
+               ("M-:" . grammatical-edit-jump-out-pair-and-newline)))
+  :config
+  (dolist (hook nema--tree-sitter-grammatical-edit-lang-hooks)
+    (add-hook hook '(lambda ()
+                      (puni-disable-puni-mode)
+                      (grammatical-edit-mode 1)))))
 
 
 ;;; tree-sitter.el ends here
