716ca21f125dea4abc723b9659f63ed7ecdf62d6
Nyk Ma <i@nyk.ma>
2022-02-07 13:41:27 +0800

    [!] my/font: fix `emacs --daemon' start error

--- my-sample/font.el
+++ my-sample/font.el
@@ -9,29 +9,29 @@
                         (fixed . "Sarasa Mono SC")
                         (variable . "Bembo Std"))
   "Fonts to use.")
-(defvar nema-font-size 12 "Font size")
+(defvar nema--font-size 12 "Font size")
 
-(defun nema--get-font-family (key)
+(defun nema//get-font-family (key)
   (alist-get key nema-fonts))
 
-(defun nema--get-font-spec (key)
+(defun nema//generate-font-spec (key)
   (format "%s-%d"
-          (nema--get-font-family key)
-          nema-font-size))
+          (nema//get-font-family key)
+          nema--font-size))
 
-(defun nema-load-base-font ()
+(defun nema//load-base-font ()
   "Load the default font for ascii characters."
-  (let* ((font-spec (nema--get-font-spec 'default)))
+  (let* ((font-spec (nema//generate-font-spec 'default)))
     (set-frame-parameter nil 'font font-spec)
     (add-to-list 'default-frame-alist (cons 'font font-spec))))
 
-(defun nema-load-face-font ()
+(defun nema//load-face-font ()
   "Load fonts used in faces.
 
 This function must be called after frame creation."
-  (let ((modeline-font-spec (nema--get-font-spec 'modeline))
-        (variable-font-spec (nema--get-font-spec 'variable))
-        (fixed-font-spec (nema--get-font-spec 'fixed)))
+  (let ((modeline-font-spec (nema//generate-font-spec 'modeline))
+        (variable-font-spec (nema//generate-font-spec 'variable))
+        (fixed-font-spec (nema//generate-font-spec 'fixed)))
     (set-face-attribute 'variable-pitch nil :font variable-font-spec :height 1.1)
     (set-face-attribute 'fixed-pitch nil :font fixed-font-spec)
     (set-face-attribute 'fixed-pitch-serif nil :font fixed-font-spec)
@@ -39,18 +39,24 @@ This function must be called after frame creation."
     (set-face-attribute 'mode-line-inactive nil :font modeline-font-spec)
     (set-face-attribute 'tab-bar nil :font modeline-font-spec)))
 
-(defun nema-load-ext-font ()
+(defun nema//load-ext-font ()
   "Load fonts used for non-ascii characters.
 
 This function must be called after frame creation."
   (let ((font (frame-parameter nil 'font))
-        (font-spec (font-spec :family (nema--get-font-family 'unicode))))
+        (font-spec (font-spec :family (nema//get-font-family 'unicode))))
     (dolist (charset '(kana han hangul cjk-misc bopomofo symbol))
       (set-fontset-font font charset font-spec))))
 
-(nema-load-base-font)
+(defun nema/load-font()
+  "Load all font configuration."
+  (interactive)
+  (when (display-graphic-p)
+    (nema//load-base-font)
+    (nema//load-ext-font)
+    (nema//load-face-font)))
 
-(add-hook 'after-init-hook
-          (lambda ()
-            (nema-load-ext-font)
-            (nema-load-face-font)))
+(if (daemonp)
+    (add-hook 'server-after-make-frame-hook #'nema/load-font)
+  ;; Else: not in daemon
+  (add-hook 'after-init-hook #'nema/load-font))
