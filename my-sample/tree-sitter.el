f5b2e910322bc3d1318ffdb4fdf6a54274889470
Nyk Ma <i@nyk.ma>
2023-01-03 17:14:53 +0800

    [#] my/tree-sitter: use native `treesit` when possible

--- my-sample/tree-sitter.el
+++ my-sample/tree-sitter.el
@@ -1,94 +1,76 @@
 ;;; nema-tree-sitter --- Incremental parsing
 ;;; Commentary:
-"Incremental parsing"
+"Incremental parsing.  Emacs will have internal `treesit' support
+since 29.1.  This script will automaticlly detect current emacs
+version and choose internal impl if possible."
 ;;; Code:
 
-(defvar nema--tree-sitter-grammatical-edit-lang-hooks
-  '(c-mode-common-hook
-    c-mode-hook
-    c++-mode-hook
-    java-mode-hook
-    haskell-mode-hook
-    ;; emacs-lisp-mode-hook
-    lisp-interaction-mode-hook
-    lisp-mode-hook
-    maxima-mode-hook
-    ielm-mode-hook
-    sh-mode-hook
-    makefile-gmake-mode-hook
-    php-mode-hook
-    python-mode-hook
-    js-mode-hook
-    go-mode-hook
-    qml-mode-hook
-    jade-mode-hook
-    css-mode-hook
-    ruby-mode-hook
-    coffee-mode-hook
-    rust-mode-hook
-    qmake-mode-hook
-    lua-mode-hook
-    swift-mode-hook
-    minibuffer-inactive-mode-hook
-    typescript-mode-hook
-    tuareg-mode-hook
-    elixir-mode-hook)
-  "All language mode hooks to use tree-sitter grammatical-edit (instead of puni-mode).")
+(require 'nema-customize-group)
 
-;; tree-sitter-core
-(use-package tree-sitter
-  :demand t)
+(if (and (featurep 'treesit)
+         (treesit-available-p)
+         (treesit-ready-p 'bash))
+    ;; NOTICE: you should compile `libtree-sitter-*.so' (or `dll' or `dylib') by your own:
+    ;; 1. git clone https://github.com/casouri/tree-sitter-module.git
+    ;; 2. cd into it and run `./batch.sh'
+    ;; 3. Copy every file under `dist/' to `~/.emacs.d/.cache/tree-sitter'.
+    (progn
+      (setq treesit-extra-load-path (list (expand-file-name ".cache/tree-sitter" user-emacs-directory)))
+      (defvar nema--tree-sitter-major-mode-map
+            '((sh-mode . bash-ts-mode)
+              (c-mode . c-ts-mode)
+              (c++-mode . c++-ts-mode)
+              (csharp-mode . csharp-ts-mode)
+              (cmake-mode . cmake-ts-mode)
+              (css-mode . css-ts-mode)
+              (dockerfile-mode . dockerfile-ts-mode)
+              ;; (elixir-mode . elixir-ts-mode)
+              ;; (glsl-mode . glsl-ts-mode)
+              (go-mode . go-ts-mode)
+              ;; (heex-mode . heex-ts-mode)
+              ;; (html-mode . html-ts-mode)
+              (java-mode . java-ts-mode)
+              (javascript-mode . js-ts-mode)
+              (js-json-mode . json-ts-mode)
+              ;; (julia-mode . julia-ts-mode)
+              ;; (makefile-gmake-mode . makefile-ts-mode)
+              ;; (markdown-mode . markdown-ts-mode)
+              ;; (org-mode . org-ts-mode)
+              ;; (perl-mode . perl-ts-mode)
+              (ruby-mode . ruby-ts-mode)
+              (rust-mode . rust-ts-mode)
+              ;; (sql-mode . sql-ts-mode)
+              (conf-toml-mode . toml-ts-mode)
+              (rjsx-mode . tsx-ts-mode)
+              (typescript-mode . typescript-ts-mode)
+              ;; (verilog-mode . verilog-ts-mode)
+              ;; (vhdl-mode . vhdl-ts-mode)
+              ;; (wgsl-mode . wgsl-ts-mode)
+              (yaml-mode . yaml-ts-mode))
+            "Mode mapping for all major modes that support `treesit'.")
+      (dolist (modemap nema--tree-sitter-major-mode-map)
+        (push modemap major-mode-remap-alist)
+        (if nema-lsp
+            (add-hook (intern (format "%s-hook" (cdr modemap))) #'lsp))))
 
-(use-package tree-sitter-langs
-  :after tree-sitter
-  :config
-  (require 'tree-sitter-hl)
-  (require 'tree-sitter-query)
-  (global-tree-sitter-mode)
-  (delight 'tree-sitter-mode nil 'tree-sitter)
-  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
+  ;; else
+  ;; tree-sitter-core
+  (use-package tree-sitter
+    :demand t)
 
-  (add-to-list 'tree-sitter-major-mode-language-alist '(ng2-ts-mode . typescript)))
+  (use-package tree-sitter-langs
+    :after tree-sitter
+    :config
+    (require 'tree-sitter-hl)
+    (require 'tree-sitter-query)
+    (global-tree-sitter-mode)
+    (delight 'tree-sitter-mode nil 'tree-sitter)
+    (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
 
-(use-package tree-sitter-indent
-  :after tree-sitter)
+    (add-to-list 'tree-sitter-major-mode-language-alist '(ng2-ts-mode . typescript)))
 
-;; Structure editing based on tree-sitter
-;; (use-package grammatical-edit
-;;   :quelpa (grammatical-edit :fetcher github :repo "manateelazycat/grammatical-edit")
-;;   :demand t
-;;   :bind ((:map grammatical-edit-mode-map
-;;                ("(" . grammatical-edit-open-round)
-;;                ("[" . grammatical-edit-open-bracket)
-;;                ("{" . grammatical-edit-open-curly)
-;;                (")" . grammatical-edit-close-round)
-;;                ("]" . grammatical-edit-close-bracket)
-;;                ("}" . grammatical-edit-close-curly)
-;;                ("=" . grammatical-edit-equal)
+  (use-package tree-sitter-indent
+    :after tree-sitter))
 
-;;                ("%" . grammatical-edit-match-paren)
-;;                ("\"" . grammatical-edit-double-quote)
-
-;;                ("SPC" . grammatical-edit-space)
-;;                ("RET" . grammatical-edit-newline)
-
-;;                ("M-o" . grammatical-edit-backward-delete)
-;;                ("C-d" . grammatical-edit-forward-delete)
-;;                ("C-k" . grammatical-edit-kill)
-
-;;                ("M-\"" . grammatical-edit-wrap-double-quote)
-;;                ("M-[" . grammatical-edit-wrap-bracket)
-;;                ("M-{" . grammatical-edit-wrap-curly)
-;;                ("M-(" . grammatical-edit-wrap-round)
-;;                ("M-)" . grammatical-edit-unwrap)
-
-;;                ("M-p" . grammatical-edit-jump-right)
-;;                ("M-n" . grammatical-edit-jump-left)
-;;                ("M-:" . grammatical-edit-jump-out-pair-and-newline)))
-;;   :config
-;;   (dolist (hook nema--tree-sitter-grammatical-edit-lang-hooks)
-;;     (add-hook hook #'(lambda ()
-;;                       (puni-disable-puni-mode)
-;;                       (grammatical-edit-mode 1)))))
 
 ;;; tree-sitter.el ends here
