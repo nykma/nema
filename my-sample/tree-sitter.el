b4b071e6df9791058be1d6a6c6c990d5cf7a7b1f
Nyk Ma <i@nyk.ma>
2023-08-30 01:44:56 +0800

    [#] my/tree-sitter: use Emacs internal `treesit' if possible

--- my-sample/tree-sitter.el
+++ my-sample/tree-sitter.el
@@ -8,57 +8,40 @@ version and choose internal impl if possible."
 (require 'nema-customize-group)
 
 (if (and (featurep 'treesit)
-         (treesit-available-p)
-         (treesit-ready-p 'bash))
+         (treesit-available-p))
     ;; NOTICE: you should compile `libtree-sitter-*.so' (or `dll' or `dylib') by your own:
     ;; 1. git clone https://github.com/casouri/tree-sitter-module.git
     ;; 2. cd into it and run `./batch.sh'
     ;; 3. Copy every file under `dist/' to `~/.emacs.d/.cache/tree-sitter'.
     (progn
-      (setq treesit-extra-load-path (list (expand-file-name ".cache/tree-sitter" user-emacs-directory))
-            treesit-font-lock-level 4)
-      (defvar nema--tree-sitter-major-mode-map
-            '((sh-mode . bash-ts-mode)
-              (c-mode . c-ts-mode)
-              (c++-mode . c++-ts-mode)
-              (csharp-mode . csharp-ts-mode)
-              (cmake-mode . cmake-ts-mode)
-              (css-mode . css-ts-mode)
-              (dockerfile-mode . dockerfile-ts-mode)
-              ;; (elixir-mode . elixir-ts-mode)
-              ;; (glsl-mode . glsl-ts-mode)
-              (go-mode . go-ts-mode)
-              ;; (heex-mode . heex-ts-mode)
-              ;; (html-mode . html-ts-mode)
-              (java-mode . java-ts-mode)
-              (javascript-mode . js-ts-mode)
-              (js-json-mode . json-ts-mode)
-              ;; (julia-mode . julia-ts-mode)
-              ;; (makefile-gmake-mode . makefile-ts-mode)
-              ;; (markdown-mode . markdown-ts-mode)
-              ;; (org-mode . org-ts-mode)
-              ;; (perl-mode . perl-ts-mode)
-              (ruby-mode . ruby-ts-mode)
-              (rust-mode . rust-ts-mode)
-              ;; (sql-mode . sql-ts-mode)
-              (conf-toml-mode . toml-ts-mode)
-              (rjsx-mode . tsx-ts-mode)
-              (typescript-mode . typescript-ts-mode)
-              ;; (verilog-mode . verilog-ts-mode)
-              ;; (vhdl-mode . vhdl-ts-mode)
-              ;; (wgsl-mode . wgsl-ts-mode)
-              (yaml-mode . yaml-ts-mode))
-            "Mode mapping for all major modes that support `treesit'.")
+      (setq treesit-font-lock-level 4
+            treesit-extra-load-path (list (expand-file-name ".cache/tree-sitter" user-emacs-directory)))
+      ;; auto download and compile .so modules
+      (use-package treesit-auto
+        :config
+        (global-treesit-auto-mode)
+        (setq
+         ;; Auto install all supported languages
+         treesit-auto-install t
+         treesit-auto-langs
+         (cl-loop for recipe in treesit-auto-recipe-list
+                  collect (treesit-auto-recipe-lang recipe))
+         treesit-language-source-alist (treesit-auto--build-treesit-source-alist))
+        ;; Add LSP and combobulate support for each of `-ts-mode' hooks.
+        (when (eq nema-lsp 'lsp-mode)
+          (let* ((tree-sitter-modes (cl-loop for recipe in treesit-auto-recipe-list
+                                             collect (treesit-auto-recipe-ts-mode recipe)))
+                 (tree-sitter-mode-hooks (cl-loop for mode in tree-sitter-modes
+                                                  collect (intern (format "%s-hook" mode)))))
+            ;; Iter each hooks in `tree-sitter-mode-hooks' and add `lsp' to it.
+            (dolist (hook tree-sitter-mode-hooks)
+              (add-hook hook #'lsp)
+              (add-hook hook #'combobulate-mode)))))
       ;; Structure editing
       (use-package combobulate
-        :quelpa (combobulate :fetcher github :repo "mickeynp/combobulate"))
-      (dolist (modemap nema--tree-sitter-major-mode-map)
-        (push modemap major-mode-remap-alist)
-        (if nema-lsp
-            (add-hook (intern (format "%s-hook" (cdr modemap))) #'lsp))
-        (add-hook (intern (format "%s-hook" (cdr modemap))) #'combobulate-mode)))
+        :quelpa (combobulate :fetcher github :repo "mickeynp/combobulate")))
 
-  ;; else
+  ;; else: treesit is not internal function of Emacs - use tree-sitter.el
   ;; tree-sitter-core
   (use-package tree-sitter
     :demand t)
